e44f49194f821501b8be9c7087fb606ab77764e8
Poi1802 <122525884+Poi1802@users.noreply.github.com>
2023-01-18 18:55:24 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+   $('.bike-slider, .slider__items').slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+   });
+});
